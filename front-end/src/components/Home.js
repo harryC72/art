32fc6e324291f23811612b8babf0358c896df9a7
HarryC72 <haraldcarlsten@yahoo.se>
2021-04-25 16:09:16 +0200

    starting to implement the home page with its blog

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import { Container, Card, Button } from 'react-bootstrap';
+
+const homeStyle = {
+  width: '90%',
+  padding: '10px',
+  boxSizing: 'border-box',
+  display: 'flex',
+};
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Card style={homeStyle}>
+        <Card.Img variant='top' src='holder.js/100px180' />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant='primary'>Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 };
